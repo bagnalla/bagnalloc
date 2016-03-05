d94754362b7408897ae0ba7f6d378ced8c355b35
Alexander Bagnall <ab667712@ohio.edu>
2016-03-05 00:40:48 -0500

    Changed README.

--- malloc.c
+++ malloc.c
@@ -4,9 +4,8 @@
  * @date March 4, 2016
  * @brief File containing implementations of malloc(), free(), calloc(), and realloc().
  *
- * With these memory allocation functions, all dynamic memory is allocated in heap which is implemented as a doubly-linked list.
- * They can be used as a general-purpose replacement for the equivalent standard library functions (or so I think - may be unsafe in some way).
- * In my testing this implementation seems perform equal or better than the standard implementation except for heavy parallel allocation (less efficient usage of mutex I guess)
+ * These functions can be used as a general-purpose replacement for the equivalent standard library functions (or so I think - may be unsafe in some way).
+ * In my testing this implementation seems perform the same or better than the standard implementation except for heavy parallel allocation (less efficient usage of mutex I guess)
  * The memory overhead for block metadata is also larger than the standard implementation which is most noticeable when doing a lot of small allocations.
  * The heap size is managed via the glibc sbrk() function. Allocation requests >= 128kB are allocated via mmap instead of growing the heap.
  * Thread safety is guaranteed via a pthread mutex.
